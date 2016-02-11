f4f82fb6a756eb2c218eee2aada149ee87459062
Issac-Kim <ikim1@stuy.edu>
2016-02-11 00:48:35 -0500

    add add/remove queen functions and have buggy solves

--- 02nqueens/Board.java
+++ 02nqueens/Board.java
@@ -1,6 +1,6 @@
 public class Board{
     int[][] board;
-    public board(int n){
+    public Board(int n){
 	board = new int[n][n];
 	for(int i = 0; i < board.length;i++){
 	    for(int k = 0; k < board[i].length; k++){
@@ -9,22 +9,67 @@ public class Board{
 	}
     }
     public boolean solve(){
-	
+	return solveH(0);
     }
-    public void addQueen(int row, int col){
-	board[row][col] = 1;
-	for(int i = row + 1; i < board.length; i++){
-	    board[i][col] -= 1;
-	    if(col != 0){
-		for(int k = col - 1; k > -1; k--){
-		    board[i][k] -= 1;
-		}
-	    }
-	    if(col != board.length - 1){
-		for(int j = col + 1; j < board.length; j++){
-		    board[i][j] -= 1;
-		}
+    public boolean solveH(int row){
+	for(int c = 0; c < board.length; c++){
+	    if(board[board.length-1][c] == 1){
+		return true;
 	    }
 	}
+	for(int c = 0; c < board.length; c++){
+	    System.out.println(toString());
+	    if(addQueen(row,c)){
+		solveH(row + 1);
+	    }
+	    else{
+		removeQueen(row,c);
+	    }
+	}
+	return false;
+    }
+    public boolean addQueen(int row, int col){
+	if(board[row][col]!= 0){
+	    return false;
+	}
+	board[row][col] = 1;
+	for(int i = row + 1; i < board.length; i++){
+	    board[i][col]--;
+	    if(col-(i-row-1) > 0){	
+		board[i][col-(i-row)]--;
+	    }
+	    if(col+(i-row-1) < board.length - 1){
+		board[i][col+(i-row)]--;
+	    }
+	}
+	return true;
+    }
+    public boolean removeQueen(int row, int col){
+	if(board[row][col]!= 1){
+	    return false;
+	}
+	board[row][col] = 0;
+	for(int i = row + 1; i < board.length; i++){
+	    board[i][col]++;
+	    if(col-(i-row-1) > 0){	
+		board[i][col-(i-row)]++;
+	    }
+	    if(col+(i-row-1) < board.length - 1){
+		board[i][col+(i-row)]++;
+	    }
+	}
+	return true;
+    }
+    public String toString(){
+	String ans = "";
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board[0].length; c++){
+		ans+= board[r][c] + "\t";
+	    }
+	    ans+="\n";
+	}
+	return ans;
     }
 }
+    
+    
---
+++ 02nqueens/Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+    public static void main(String[] args){
+	Board b = new Board(2);
+	System.out.println(b);
+	System.out.println(b.solve());
+	System.out.println(b);
+    }
+}
