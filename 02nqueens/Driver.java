f2b857019b7bb4d5f462efea07aac309d20e09ae
Issac-Kim <ikim1@stuy.edu>
2016-02-12 12:20:20 -0500

    make constructor for knight

--- 02nqueens/Driver.java
+++ 02nqueens/Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[] args){
-	QueenBoard b = new QueenBoard(8);
+	QueenBoard b = new QueenBoard(50);
 	System.out.println(b);
 	//System.out.println(b.addQueen(1,1));
 		System.out.println(b.solve());
---
+++ 03knightboard/KnightBoard.java
@@ -0,0 +1,16 @@
+public class KnightBoard{
+    int[][] board;
+    public KnightBoard(int n){
+	board = new int[n+4][n+4];
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board.length; c++){
+		if(r < 2 || r > board.length - 3 || c < 2 || c >board.length -3){
+		    board[r][c] = -1;
+		}
+		else{
+		    board[r][c] = 1;
+		}
+	    }
+	}
+    }
+}
