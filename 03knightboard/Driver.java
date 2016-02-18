ee508a20cf6b4ec4b1fe88fcacba0725998362d7
Issac-Kim <ikim1@stuy.edu>
2016-02-18 16:53:15 -0500

    add solve and helper functions

--- 03knightboard/Driver.java
+++ 03knightboard/Driver.java
@@ -1,7 +1,8 @@
 public class Driver{
     public static void main(String[] args){
-	KnightBoard b = new KnightBoard(3);
+	KnightBoard b = new KnightBoard(5);
 	System.out.println(b);
+	System.out.println(b.solve());
 	b.printSolution();
     }
 }
--- 03knightboard/KnightBoard.java
+++ 03knightboard/KnightBoard.java
@@ -1,5 +1,6 @@
 public class KnightBoard{
     int[][] board;
+    int size;
     public KnightBoard(int n){
 	board = new int[n+4][n+4];
 	for(int r = 0; r < board.length; r++){
@@ -12,6 +13,119 @@ public class KnightBoard{
 		}
 	    }
 	}
+	size = n * n;
+    }
+    public void solve(){
+	solveH(board.length/2,board.length/2, 1);
+    }
+    public boolean solveH(int row, int col, int n){
+	if(moveKnight(row+2,col+1,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row+2,col+1,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row+2,col+1);
+	    }
+	}
+	if(moveKnight(row+2,col-1,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row+2,col-1,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row+2,col-1);
+	    }
+	}
+	if(moveKnight(row-2,col+1,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row-2,col+1,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row-2,col+1);
+	    }
+	}
+	if(moveKnight(row-2,col-1,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row-2,col-1,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row-2,col-1);
+	    }
+	}
+	if(moveKnight(row+1,col+2,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row+2,col+2,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row+1,col+2);
+	    }
+	}
+	if(moveKnight(row-1,col+2,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row-1,col+2,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row-1,col+2);
+	    }
+	}
+	if(moveKnight(row+1,col-2,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row+1,col-2,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row+1,col-2);
+	    }
+	}
+	if(moveKnight(row-1,col-2,n)){
+	    if(n == size){
+		return true;
+	    }
+	    if(solveH(row-1,col-2,n++)){
+		return true;
+	    }
+	    else{
+		removeKnight(row-1,col-2);
+	    }
+	}
+	else{
+	    return false;
+	}
+
+    }
+    
+    public boolean moveKnight(int r, int c, int n){
+	if(board[r][c] != 0){
+	    return false;
+	}
+	else{
+	    board[r][c] = n;
+	    return true;
+	}
+    }
+    public int removeKnight(int r, int c){
+	int temp = board[r][c];
+	board[r][c] = 0;
+	return temp;
     }
     public void printSolution(){
 	for(int r = 0; r < board.length; r++){
