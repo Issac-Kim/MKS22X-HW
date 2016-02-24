676b544f58c9ab042c5283cdbf9062dcf1de85a1
Issac-Kim <ikim1@stuy.edu>
2016-02-23 22:59:06 -0500

    instantiate board with elevation levels

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -2,12 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Bronze{
-    int row;
-    int col;
-    int depth;
-    int stomps;
     public static void main(String[] args){
-	file f = new file("makelake.in");
-	Scanner lines = new scanner(f);
+	int row;
+	int col;
+	int depth;
+	int stomps;
+	try{
+	    File f = new File("makelake.in");
+	    Scanner lines = new Scanner(f);
+	    Scanner info = new Scanner(lines.nextLine());
+	    row = info.nextInt();
+	    col = info.nextInt();
+	    depth = info.nextInt();
+	    stomps = info.nextInt();
+	    int[][] land = new int[row][col];
+	    for(int r = 0; r < row; r++){
+		Scanner vals = new Scanner(lines.nextLine());
+		for(int c = 0; c < col; c++){
+		    land[r][c] = vals.nextInt();
+		}
+	    }
+	    for(int i = 0; i < row; i++){
+		for(int j = 0; j < col; j++){
+		    System.out.print(land[i][j] + " ");
+		}
+		System.out.println("");
+	    }
+	}catch(FileNotFoundException e){
+	    System.out.println("no file found");
+	}
+	
     }
 }
---
+++ 04USACO/Bronze.java~
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Bronze{
+    int row;
+    int col;
+    int depth;
+    int stomps;
+    public static void main(String[] args){
+	file f = new file("makelake.in");
+	Scanner lines = new scanner(f);
+    }
+}
