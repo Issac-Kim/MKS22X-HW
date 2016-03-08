6cdd5a406f1de6920cd09578195a9df468e353d2
Issac-Kim <ikim1@stuy.edu>
2016-03-08 16:13:51 -0500

    finish mergesort

--- 06sorts/Driver.java
+++ 06sorts/Driver.java
@@ -3,8 +3,10 @@ public class Driver{
 	int[] a = {1, 2, 3, 6, 7};
 	int[] b = {2, 10};
 	int[] arr = Sorts.merge(a, b);
-	for(int i = 0; i < arr.length; i++){
-	    System.out.print(arr[i] + ",");
+	int[] ary = {321, 2, 14, 54, -12, 0, 213, 213, 15,13213};
+	Sorts.mergesort(ary );
+	for(int i = 0; i < ary.length; i++){
+	    System.out.print(ary[i] + ",");
 	}
     }
 }
--- 06sorts/Sorts.java
+++ 06sorts/Sorts.java
@@ -1,6 +1,23 @@
-public  class Sorts{
-    public void mergeSort(int[] ary){
-	mergeH(ary, 0
+public class Sorts{
+    public static void mergesort(int[] ary){
+	int data[] = mergeH(ary, 0, ary.length - 1);
+	for(int i = 0; i < data.length; i++){
+	    ary[i] = data[i];
+	}
+    }
+    public static int[] mergeH(int[] ary, int start, int end){
+	//System.out.println(start + "," + end);
+	if(start == end){
+	    int[]arr= new int[1];
+	    arr[0] = ary[start];
+	    return arr;
+	}
+	int[] a = mergeH(ary, start, (end-start)/2  + start);
+	int[] b = mergeH(ary, (end-start)/2 + start + 1, end);
+	int data[] = merge(a,b);
+	return data;
+   
+	
     }
     public static int[] merge(int[] a, int[] b){
 	int A = 0;
@@ -27,5 +44,8 @@ public  class Sorts{
 	    i++;
 	}
 	return arr;
+    }
+    public static String name(){
+	return "6,Kim,Issac";
     }
 }
