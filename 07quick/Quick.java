9dbeadd7fd47c9815e1a778b995e3d34c035cca8
Issac-Kim <ikim1@stuy.edu>
2016-03-08 22:40:31 -0500

    start quick select

--- 07quick/Quick.java
+++ 07quick/Quick.java
@@ -1,9 +1,24 @@
 import java.util.*;
 
 public class Quick{
-    public static int partition(int left, int right){
+    public static int partition(int[] data, int left, int right){
 	Random rand = new Random();
 	int index = rand.nextInt(right - left + 1) + left;
+	int val = data[index];
+	for(int i = left; i < right + 1; i++){
+	    if(data[i] < val && i > index){
+		int temp = data[i];
+		data[i] = val;
+		val = temp;
+		index = i;
+	    }
+	    else if(data[i] > val && i < index){
+		int temp = data[i];
+		data[i] = val;
+		val = temp;
+		index = i;
+	    }
+	}
 	return index;
     }
 }
