43df02df14de3369d2ef56031e61421ef86fc531
Issac-Kim <ikim1@stuy.edu>
2016-03-09 01:26:02 -0500

    work on recursive quick select func, still has bugs

--- 07quick/Driver.java
+++ 07quick/Driver.java
@@ -1,7 +1,10 @@
 public class Driver{
     public static void main(String[] args){
-	for(int i = 0; i < 20; i++){
-	    System.out.println(Quick.partition(4,10)); 
+	int[] data = {1,321,-12,0,123,4,51,23,42,124};
+	for(int i = 0; i < data.length; i++){
+	    data = new int[]{1,321,-12,0,123,4,51,23,42,124};
+	    System.out.println(Quick.quickselect(data, i));
 	}
+	
     }
 }
