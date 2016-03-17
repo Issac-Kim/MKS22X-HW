ee634ff8ba2147d03062d78f4f83274fbc7f61cd
Issac-Kim <ikim1@stuy.edu>
2016-03-16 23:33:27 -0400

    work at home

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -16,8 +16,34 @@ public class MyLinkedList{
 	size++;
 	return true;
     }
+    public boolean add(int index, int val){
+	LNode current = start;
+	for(int i = 0; i < index; i++){
+	    current = current.getNext();
+	}
+	int temp = current.get();
+	LNode next = current.getNext();
+	current.set(val);
+	current = current.getNext();
+	current.set(temp);
+	current.setNext(next);
+	size++;
+	return true;
+    }
     public int remove(int index){
-	
+	LNode current = start;
+	for(int i = 0; i < index; i++){
+	    current = current.getNext();
+	}
+	int temp = current.get();
+	LNode next = current.getNext();
+	LNode head = start;
+	for(int i = 0; i < index - 1; i++){
+	    head = head.getNext();
+	}
+	head.setNext(next);
+	size--;
+	return temp;
     }
     public String toString(){
 	LNode current = start;
@@ -29,7 +55,40 @@ public class MyLinkedList{
 	}
 	return s;
     }
-  
+    public int get(int index){
+	LNode current = start;
+	for(int i = 0; i < index; i++){
+	    current = current.getNext();
+	}
+	return current.get();
+    }
+    public int set(int index, int newVal){
+	LNode current = start;
+	for(int i = 0; i < index; i++){
+	    current = current.getNext();
+	}
+	int temp = current.get();
+	current.set(newVal);
+	return temp;
+    }
+    public int size(){
+	return size;
+    }
+    public int indexOf(int val){
+	LNode current = start;
+	for(int i = 0; i < size; i++){
+	    if(current.get() == val){
+		return i;
+	    }
+	    current = current.getNext();
+	}
+	if(current.get() != val){
+	    return -1;
+	}
+	else{
+	    return size - 1;
+	}
+    }
     private class LNode{
 	int val;
 	LNode next;
