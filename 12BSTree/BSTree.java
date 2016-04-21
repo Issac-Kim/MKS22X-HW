38ccc636801f7607f4fedf68cc468fd24abe959f
Issac Kim <ikim1@stuy.edu>
2016-04-21 12:19:50 -0400

    make add method, not tested yet

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -1,9 +1,37 @@
 public class BSTree<T extends Comparable<T>>{
     Node root;
+
+    public BSTree(Node root){
+	this.root = root;
+    }
+    public BSTree(){
+    }
+    public void add(T value){
+	add(value, root);
+    }
+    public void add(T value, Node localRoot){
+	if(localRoot == null){
+	    localRoot = new Node(value);
+	}
+	else if(value.compareTo(localRoot.getData()) < 0){
+	    add(value, localRoot.getLeft());
+	}
+	else{
+	    add(value, localRoot.getRight());
+	}
+    }
     
     public class Node{
 	Node left, right;
 	T data;
+	public Node(T value){
+	    data = value;
+	}
+	public Node(T value, Node left, Node right){
+	    data = value;
+	    this.left = left;
+	    this.right = right;
+	}
 	public Node getLeft(){
 	    return left;
 	}
@@ -22,5 +50,16 @@ public class BSTree<T extends Comparable<T>>{
 	public void setData(T data){
 	    this.data = data;
 	}
+	public int numChildren(){
+	    int num = 0;
+	    if(getLeft() != null){
+		num++;
+	    }
+	    if(getRight() != null){
+		num++;
+	    }
+	    return num;
+	}
     }
+
 }
