98d99ed3df3789fa4551a7b578bcb2e94b0ad0c9
Issac Kim <ikim1@stuy.edu>
2016-05-09 12:17:20 -0400

    fix files

--- 13heap/MyHeap.java
+++ 13heap/MyHeap.java
@@ -1,23 +1,32 @@
 import java.util.*;
-@SuppressWarnings("unchecked")
+ @SuppressWarnings({"unchecked"})
+
 
 public class MyHeap<T extends Comparable<T>>{
     private T[] data;
     private int size;
     boolean isMax = true;
-    
     public MyHeap(){
-	data = new T[10];
+	data = (T[]) new Comparable[10];
 	size = 0;
     }
     public MyHeap(T[] Array){
-	data = heapify(Array);
+	data = (T[]) new Comparable[Array.length + 1];
 	size = Array.length;
+	heapify(Array);
+	
     }
-
+    private boolean compare(T x, T y){
+	if(isMax){
+	    return x.compareTo(y) > 0;
+	}
+	else{
+	    return x.compareTo(y) < 0;
+	}
+    }
     private void pushDown(int k){
 	T temp = data[k];
-	if(data[k*2].compareTo(data[k*2+1]) < 0){
+	if(!compare(data[k*2],data[k*2+1])){
 	    data[k] = data[k*2+1];
 	    data[k*2+1] = temp;
 	}
@@ -31,24 +40,86 @@ public class MyHeap<T extends Comparable<T>>{
 	data[k] = data[k/2];
 	data[k/2] = temp;
     }
-    private void heapify(T[] arr){
+    public void heapify(T[] arr){
+	for(int i = 0; i < arr.length; i++){
+	    data[i + 1] = arr[i];
+	}
+	if(size > 1){
+	    int index = size / 2;
+	    while(index != 1){
+		for(int i = 0; i < index; i++){
+		    if(((index + i) * 2 +1 <= size)&&(!compare(data[index + i], data[(index + i) * 2]) ||!compare(data[index + i], data[(index + i) *2 + 1]))){
+			pushDown(index + i);
+		    }
+		}
+		index = index / 2;
+		
+	    }
 	
+	    
+	}
     }
     public T delete(){
-	return data[0];
+	T temp = data[1];
+	if(size == 1){
+	    data[1] = null;
+	    size--;
+	}
+	else{
+	    data[1] = null;
+	    int index = 1;
+	    while(index < size/2){
+		if(compare(data[index * 2], data[index * 2 + 1])){
+		    index = index * 2;
+		}
+		else{
+		    index = index * 2 + 1;
+		}
+		pushUp(index);
+		
+	    }
+	    size--;
+	}
+	return temp;
     }
     public void add(T x){
 	if(size == data.length -1){
 	    doubleSize();
 	}
 	if(size == 0){
-	    data[1] = T;
+	    data[1] = x;
+	    size++;
 	}
+	else{
+	    data[size + 1] = x;
+	    int index = size + 1;
+	    while(index > 1 && compare(data[index], data[index/2])){
+		pushUp(index);
+		index = index/2;
+	    }
+	    size++;
+	}
+	    
     }
-    private void doubleSize(){
-
+    public T peek(){
+	return data[1];
+    }
+        private void doubleSize(){
+	T[] temp = (T[]) new Comparable[data.length];
+	for(int i = 0; i < data.length; i++){
+	    temp[i] = data[i];
+	}
+	data = (T[]) new Comparable[data.length * 2];
+	for(int i = 1; i < temp.length; i++){
+	    data[i] = temp[i];
+	}
+	
     }
     public String toString(){
-	return "";
+	String ans = "";
+	for(int i = 1; i < size + 1; i++){
+	    ans += data[i] + " ";
+	}
+	return ans;
     }
 }
